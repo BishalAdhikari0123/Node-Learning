d1e49db01746cab2f7d44b8dd2b9010bce676a3a
BishalAdhikari0123 <117503902+BishalAdhikari0123@users.noreply.github.com>
2024-12-12 14:26:32 +0545

    Initialize

---
+++ hello.js
@@ -0,0 +1,21 @@
+// const { stdout } = require("process")
+// const readline=require("readline")
+
+// const read=readline.createInterface({
+//    input:process.stdin,
+//    output:process.stdout
+// })
+
+// read.question("Enter your name:",(name)=>{
+//    console.log("You entered:"+name)
+//    read.close()
+// })
+`
+// read.on("close",()=>{
+//    console.log("interfaced close")
+//    process.exit(0)
+// })
+
+// const fs=require("fs")
+// const http=require("http")
+// const path=require("path")
