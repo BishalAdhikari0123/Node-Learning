b74932e114e8dc8bfdec55d17ef8171c4cc929aa
BishalAdhikari0123 <117503902+BishalAdhikari0123@users.noreply.github.com>
2024-12-12 16:13:37 +0545

    sdj

--- hello.js
+++ hello.js
@@ -1,21 +1,4 @@
-// const { stdout } = require("process")
-// const readline=require("readline")
-
-// const read=readline.createInterface({
-//    input:process.stdin,
-//    output:process.stdout
-// })
-
-// read.question("Enter your name:",(name)=>{
-//    console.log("You entered:"+name)
-//    read.close()
-// })
-
-// read.on("close",()=>{
-//    console.log("interfaced close")
-//    process.exit(0)
-// })
-
-// const fs=require("fs")
-// const http=require("http")
-// const path=require("path")
+const args = process.argv;
+console.log(args)
+const name = args[2];
+console.log("Hello", name);
