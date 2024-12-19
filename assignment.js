dde05231e2c448bfd31078ed765b32919e242cc1
BishalAdhikari0123 <117503902+BishalAdhikari0123@users.noreply.github.com>
2024-12-19 21:10:31 +0545

    multi dimen pattern

--- assignment.js
+++ assignment.js
@@ -3,7 +3,48 @@
 // 30 29 28
 // 20 19 18 17
 // 10 9  8  7  6
+let data = 50;
+let dec1 = 10;
+let dec2 = 1;
 
+for (let i = 0; i < 5; i++) {
+    let row = "";
+    let tempData = data;
+
+    for (let j = 0; j <= i; j++) {
+        switch (j) {
+            case 0:
+                row += tempData;
+                break;
+            default:
+                tempData -= dec2;
+                row += " " + tempData;
+                break;
+        }
+    }
+
+    data -= dec1;
+    console.log(row);
+}
+
+
+
+
+// for (let i = 1; i <= 5; i++) {
+//     let rows = '';
+//     for (let j = 1; j <= i; j++) {
+//       rows +=' '+'*';
+//     }
+//     console.log(rows);
+//   }
+  
+
+// for (let i = 0; i < 5; i++) {
+//     for (let j = 0; j <= i; j++) {
+//         console.log("*")
+//     }
+//     console.log("\n")
+// }
 
 
 
